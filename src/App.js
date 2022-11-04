bb88ddba3db9ab5ce766365c6cea2d011583655b
BT <bahartofigh@gmail.com>
2022-11-05 00:46:41 +0330

    this is first commit.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route , Routes , Navigate } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path='/' element={<Navigate to="/signup"/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
